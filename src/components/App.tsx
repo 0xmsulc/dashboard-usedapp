5a05329d430939553495c2e727a3fe3242c834c2
0xsulc <msulc.eth@gmail.com>
2022-11-29 19:24:26 +0100

    dashboard-usedapp: add WalletButton

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,19 @@
 import { FC, useState } from 'react';
+import ethereumLogo from '../assets/images/ethLogo.png';
 
 import { NodeUrls, useEthers } from '@usedapp/core';
 import { EtherscanProvider } from '@ethersproject/providers';
-import ethereumLogo from '../assets/images/ethLogo.png';
+
 import GasPrice from './GasPrice';
 import SearchBar from './SearchBar';
+import WalletButton from './WalletButton';
 import Avatar from '@mui/material/Avatar';
 
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import DirectionsIcon from '@mui/icons-material/Directions';
 
 type Props = {
   readOnlyUrls: NodeUrls | undefined;
@@ -32,12 +26,13 @@ const App: FC<Props> = ({ etherscanProvider, readOnlyUrls }) => {
   const isChainSupported =
     !!chainId && !!readOnlyUrls && !!readOnlyUrls[chainId];
 
-  console.log(account, isChainSupported, etherscanProvider);
+  console.log(inputAccount);
+
   return (
     <Container disableGutters={true}>
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
-          <Toolbar>
+          <Toolbar sx={{ justifyContent: 'space-between' }}>
             <IconButton
               disabled
               size="large"
@@ -48,10 +43,10 @@ const App: FC<Props> = ({ etherscanProvider, readOnlyUrls }) => {
             >
               <Avatar alt="Ethereum" src={ethereumLogo}></Avatar>
             </IconButton>
-            <Typography variant="inherit" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
+            <WalletButton
+              chainId={chainId}
+              isChainSupported={isChainSupported}
+            />
           </Toolbar>
         </AppBar>
       </Box>
---
+++ src/components/WalletButton.tsx
@@ -0,0 +1,77 @@
+import { FC, useEffect, useState } from 'react';
+import {
+  shortenAddress,
+  useEthers,
+  useEtherBalance,
+  useLookupAddress,
+} from '@usedapp/core';
+import { formatEther } from '@ethersproject/units';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const WalletButton: FC<{
+  chainId: number | undefined;
+  isChainSupported: boolean;
+}> = ({ chainId, isChainSupported }) => {
+  const [rendered, setRendered] = useState('');
+  const { account, activateBrowserWallet, deactivate, error } = useEthers();
+  const { ens } = useLookupAddress(account);
+  const balance = useEtherBalance(account);
+
+  useEffect(() => {
+    if (ens) {
+      setRendered(ens);
+    } else if (account) {
+      setRendered(shortenAddress(account));
+    } else {
+      setRendered('');
+    }
+  }, [account, ens, setRendered]);
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+      }}
+    >
+      {!isChainSupported && !!account ? (
+        <Typography sx={{ flexGrow: 1 }} variant="body2">
+          Please connect to ethereum Mainnet.
+        </Typography>
+      ) : (
+        !!balance && (
+          <Typography sx={{ flexGrow: 1 }} variant="body2">
+            ETH balance: ${parseFloat(formatEther(balance)).toFixed(4)}
+          </Typography>
+        )
+      )}
+      {!!chainId && !!account && (
+        <Typography sx={{ flexGrow: 1 }} variant="body2">
+          Chain id: {chainId}
+        </Typography>
+      )}
+      {!!error && (
+        <Typography sx={{ flexGrow: 1 }} variant="body2">
+          Error while connecting wallet: {error.message}
+        </Typography>
+      )}
+      <Button
+        color="inherit"
+        onClick={() => {
+          if (!account) {
+            activateBrowserWallet();
+          } else {
+            deactivate();
+          }
+        }}
+      >
+        {!rendered && 'Connect Wallet'}
+        {!!rendered && rendered}
+      </Button>
+    </Box>
+  );
+};
+
+export default WalletButton;
